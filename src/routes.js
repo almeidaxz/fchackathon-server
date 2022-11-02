6fb1b15b677668c0c8bb8bf7b29a86dab082f6d7
almeidaxz <almeidaxz@live.com>
2022-11-02 12:43:13 -0300

    validação para previnir cadastro de conta com email já existente implementada

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const routes = express.Router();
 
 const { SignUp } = require('./controllers/users');
+const { validateUserData } = require('./middlewares/users');
+const { userSignUpSchema } = require('./schemas/userSignUpSchema');
 
-routes.post('/signup', SignUp);
+routes.post('/signup', validateUserData(userSignUpSchema), SignUp);
 
 module.exports = routes;

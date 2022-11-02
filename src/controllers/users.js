f8854f5c667c36987f65b3180512208e261b4dbf
almeidaxz <almeidaxz@live.com>
2022-11-02 12:16:38 -0300

    implementado cadastro de usuário no banco de dados -- pendente de melhorias

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,16 @@
+const knex = require('../database/dbConnect');
+
 const SignUp = async (req, res) => {
-    return res.send('Endpoint de cadastro');
+    const { name, email, password, study_tracks } = req.body;
+
+    try {
+        const teste = await knex('users').insert({ name, email, password, study_tracks }).returning('*');
+
+        return res.status(201).json({ message: 'Cadastro realizado com sucesso!' });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: 'Erro no servidor.' });
+    }
 }
 
 module.exports = {

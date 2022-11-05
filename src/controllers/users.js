5d9c0fa70b3fb0fc3edfd018fc3634886e56fa1d
Afonso Rodrigues <afonso.rodrigues@outlook.com>
2022-11-05 18:42:29 -0300

    Revert "feat: Adiciona o método para update do user"
    This reverts commit 9ab192013c234c3f1f49cffec63f7395c02a4941.

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -90,21 +90,24 @@ const SignToTrack = async (req, res) => {
     const { user_id } = req.body;
 
     try {
-        const selectedTrack = await knex('tracks').where({ id: track_id }).first();
-        if (!selectedTrack) return res.status(404).json({ message: 'Trilha não encontrada.' });
+        const selectedTrack = await knex("tracks")
+            .where({ id: track_id })
+            .first();
+        if (!selectedTrack)
+            return res.status(404).json({ message: "Trilha não encontrada." });
 
-        await knex('user_track').insert({ track_id, user_id }).returning('*');
+        await knex("user_track").insert({ track_id, user_id }).returning("*");
 
-        return res.status(201).json({ message: 'Trilha iniciada!' });
+        return res.status(201).json({ message: "Trilha iniciada!" });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ message: 'Erro no servidor.' });
+        return res.status(500).json({ message: "Erro no servidor." });
     }
-}
+};
 
 module.exports = {
     SignUp,
     Login,
     UpdateUser,
-    SignToTrack
+    SignToTrack,
 };

e528122be6cd73f9cd37865e4d0b04625bdfefd4
Afonso Rodrigues <afonso.rodrigues@outlook.com>
2022-11-07 14:37:36 -0300

    feat: Adiciona o método para deletar um conteúdo

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -11,9 +11,14 @@ const AdminLogin = async (req, res) => {
             return res.status(401).send({ message: "Usuário não encontrado." });
         }
 
-        const decryptedPassword = await bcrypt.compare(password, adminExists.password);
+        const decryptedPassword = await bcrypt.compare(
+            password,
+            adminExists.password
+        );
         if (!decryptedPassword) {
-            return res.status(401).send({ message: "Usuário ou senha inválido." });
+            return res
+                .status(401)
+                .send({ message: "Usuário ou senha inválido." });
         }
 
         const token = jwt.sign(
@@ -38,41 +43,67 @@ const AdminAddTrack = async (req, res) => {
     const { name, status } = req.body;
 
     try {
-    
-        const trackExists = await knex('tracks').where({ name }).first();
-        if (trackExists) return res.status(400).json({ message: 'Trilha já existente' });
+        const trackExists = await knex("tracks").where({ name }).first();
+        if (trackExists)
+            return res.status(400).json({ message: "Trilha já existente" });
 
-        if (!name) return res.status(400).json({ message: 'Informe o nome da trilha.' });
+        if (!name)
+            return res
+                .status(400)
+                .json({ message: "Informe o nome da trilha." });
 
-        await knex('tracks').insert({ name, status }).returning('*');
+        await knex("tracks").insert({ name, status }).returning("*");
 
-        return res.status(201).json({ message: 'Trilha cadastrada com sucesso.' });
+        return res
+            .status(201)
+            .json({ message: "Trilha cadastrada com sucesso." });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Erro no servidor." });
     }
-}
+};
 
 const AdminAddTrackContent = async (req, res) => {
-    const { name, type, duration, complete, url, description, url_image } = req.body;
+    const { name, type, duration, complete, url, description, url_image } =
+        req.body;
 
     try {
         if (!name || !type || !duration || !url) {
-            return res.status(400).json({ message: 'Informe todos os dados obrigatórios' });
+            return res
+                .status(400)
+                .json({ message: "Informe todos os dados obrigatórios" });
         }
 
-        if (type === 'artigo' && !description || type === 'artigo' && !url_image) {
-            return res.status(400).json({ message: 'Uma descrição e a url da imagem são obrigatórias caso o tipo seja artigo.' });
+        if (
+            (type === "artigo" && !description) ||
+            (type === "artigo" && !url_image)
+        ) {
+            return res.status(400).json({
+                message:
+                    "Uma descrição e a url da imagem são obrigatórias caso o tipo seja artigo.",
+            });
         }
 
-        await knex('contents').insert({ name, type, duration, complete, url, description, url_image }).returning('*');
+        await knex("contents")
+            .insert({
+                name,
+                type,
+                duration,
+                complete,
+                url,
+                description,
+                url_image,
+            })
+            .returning("*");
 
-        return res.status(201).json({ message: 'Conteúdo cadastrado com sucesso.' });
+        return res
+            .status(201)
+            .json({ message: "Conteúdo cadastrado com sucesso." });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Erro no servidor." });
     }
-}
+};
 
 //DEVELOPMENT ONLY
 const AdminSignUp = async (req, res) => {
@@ -81,18 +112,50 @@ const AdminSignUp = async (req, res) => {
     try {
         const encryptedPassword = await bcrypt.hash(password, 10);
 
-        await knex("admins").insert({ name, email, password: encryptedPassword }).returning("*");
+        await knex("admins")
+            .insert({ name, email, password: encryptedPassword })
+            .returning("*");
 
-        return res.status(201).json({ message: "Cadastro realizado com sucesso!" });
+        return res
+            .status(201)
+            .json({ message: "Cadastro realizado com sucesso!" });
     } catch (error) {
         return res.status(500).json({ message: "Erro no servidor." });
     }
 };
 
+const DeleteContent = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        var contentExists = await knex("contents").where({ id }).first();
+
+        if (!contentExists) {
+            return res
+                .status(401)
+                .send({ message: "Conteúdo não encontrado." });
+        }
+        await knex
+            .select()
+            .table("track_content")
+            .where({ content_id: id })
+            .del();
+        await knex("contents").where({ id }).del();
+
+        return res
+            .status(201)
+            .json({ message: "Conteúdo deletada com sucesso!" });
+    } catch (error) {
+        console.log(error);
+        console.log(error);
+        return res.status(500).json({ message: "Erro no servidor." });
+    }
+};
 
 module.exports = {
     AdminSignUp,
     AdminLogin,
     AdminAddTrack,
-    AdminAddTrackContent
+    AdminAddTrackContent,
+    DeleteContent,
 };

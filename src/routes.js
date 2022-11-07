16378b97781051cab2bc58ebafb6ef698a5e0042
Afonso Rodrigues <afonso.rodrigues@outlook.com>
2022-11-07 11:00:02 -0300

    feat: Adiciona rota para deletar uma trilha

--- src/routes.js
+++ src/routes.js
@@ -22,7 +22,11 @@ const {
     validateEmailUser,
 } = require("./middlewares/users");
 const { validateAdminData } = require("./middlewares/admins");
-const { GetTracks, GetContentsToTrack } = require("./controllers/tracks");
+const {
+    GetTracks,
+    GetContentsToTrack,
+    DeleteTrack,
+} = require("./controllers/tracks");
 const { userSignUpSchema } = require("./schemas/userSignUpSchema");
 
 routes.post("/signup", validateUserData(userSignUpSchema), SignUp);
@@ -32,6 +36,7 @@ routes.delete("/user/delete/:id", DeleteUser);
 routes.post("/user/sign_track/:track_id", SignToTrack);
 routes.get("/user/tracks/:id", GetUserTracks);
 routes.get("/tracks", GetTracks);
+routes.delete("/tracks/delete/:id", DeleteTrack);
 routes.get("/:id/contents", GetContentsToTrack);
 routes.post("/admin/signup", validateAdminData(userSignUpSchema), AdminSignUp); //DEVELOPMENT ONLY
 routes.post("/admin/login", AdminLogin);

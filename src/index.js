18cdb5e67abc3b933fcfc13cc2d6e00b5531a00b
almeidaxz <almeidaxz@live.com>
2022-11-14 00:22:06 -0300

    alteração em variável de ambiente

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const swaggerDocument = require("../swagger.json");
 
 const app = express();
 
-const port = process.env.PGPORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());

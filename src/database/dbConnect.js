eb3d3449857da56dfabb2188d34da29f971842d7
almeidaxz <almeidaxz@live.com>
2022-11-13 21:25:47 -0300

    atualização de variáveis de ambiente para deploy

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    ssl: { rejectUnauthorized: false }
   }
 })
 

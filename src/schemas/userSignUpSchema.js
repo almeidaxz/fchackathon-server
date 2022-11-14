d400a7eefc240462b0b4b9641fc7ba000966255d
almeidaxz <almeidaxz@live.com>
2022-11-14 11:11:12 -0300

    fix nas validações de formulários

--- src/schemas/userSignUpSchema.js
+++ src/schemas/userSignUpSchema.js
@@ -19,7 +19,7 @@ const errorPassword = {
 };
 
 const userSignUpSchema = joi.object({
-    name: joi.string().min(4).required().messages(errorName),
+    name: joi.string().min(3).pattern(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u).required().messages(errorName),
     email: joi.string().email().required().messages(errorEmail),
     password: joi.string().min(8).trim().required().messages(errorPassword),
 });
